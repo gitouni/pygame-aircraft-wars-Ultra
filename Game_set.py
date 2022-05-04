1dac45c2f900d547aa4bfd398ee6865e60c847e4
gitouni <30369762+gitouni@users.noreply.github.com>
2022-05-04 18:11:30 +0800

    Add files via upload

--- Game_set.py
+++ Game_set.py
@@ -13,15 +13,15 @@ class game_set():
         # 战机数据
         self.player_HP_level = 5
         self.player_HP_recover = 5
-        self.player_energy_level = 3
-        self.player_energy_recover_level = 3
-        self.player_cooling_level = 3
-        self.player_cooling_recover_level = 3
+        self.player_energy_level = 5
+        self.player_energy_recover_level = 6
+        self.player_cooling_level = 5
+        self.player_cooling_recover_level = 6
         # 子弹数据
-        self.bullet_ID = 2
-        self.bullet_shooting_cd_level = 4
-        self.missile_num_level = 3
-        self.missile_shooting_cd_level = 4
+        self.bullet_ID = 0
+        self.bullet_shooting_cd_level = 6
+        self.missile_num_level = 4
+        self.missile_shooting_cd_level = 6
         self.missile_damage_level = 3
         self.missile_actime_level = 3
         self.missile_speed_max_level = 3
@@ -30,10 +30,10 @@ class game_set():
         self.player_HP_list = [5,6.5,7.5,10,15,20,30]
         self.player_HP_recover_list = [0.0005,0.001,0.002,0.005,0.01,0.025,0.1]
         self.player_energy_list = [15,30,45,65,90,120,150]
-        self.player_energy_recover_list = [0.03,0.05,0.08,0.12,0.2,0.35,0.45]
+        self.player_energy_recover_list = [0.03,0.05,0.1,0.18,0.32,0.48,0.65]
         self.player_cooling_list = [10,16,24,36,50,75,120]
-        self.player_cooling_recover_list = [0.04,0.07,0.12,0.16,0.2,0.25,0.35]
-        self.player_shooting_cd_list = [350,300,240,210,165,135,100]
+        self.player_cooling_recover_list = [0.04,0.07,0.14,0.24,0.36,0.5,0.68]
+        self.player_shooting_cd_list = [400,333,285,200,166,83,66]
         self.missile_num_list = [1,2,4,6,8]
         self.missile_cd_list = [500,450,400,350,300,250,220]
         self.missile_damage_list = [10,12,15,18,24,27,30]
